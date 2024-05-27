76547d38a66e9c604fb7348aa79a0f244d081ea0
novacvi1 <novacvi1@fel.cvut.cz>
2024-05-27 18:05:42 +0200

    Save before changing to svg

--- js/app.js
+++ js/app.js
@@ -6,16 +6,36 @@ import ToolBar from './components/ToolBar.js'
 const mindmap = new MindMap()
 const storage = new Storage()
 
-// Načtení dat z localStorage
+// Localstorage
 const savedData = storage.loadData()
 if(savedData) {
   mindmap.loadData(savedData)
 }
 
-// Inicializace komponent
 const canvas = new Canvas('mindmap-canvas', mindmap)
 const toolBar = new ToolBar(mindmap, canvas)
 
-// Nastavení událostí
 canvas.initEvents()
 toolBar.initEvents()
+
+// Pop-up info
+const infoPopup = document.getElementById('info-popup');
+const showInfoButton = document.getElementById('info-button');
+
+showInfoButton.addEventListener('click', () => {
+  infoPopup.classList.add('show');
+});
+
+// Hide the pop-up
+const closeButton = document.getElementById('close-popup');
+
+closeButton.addEventListener('click', () => {
+  infoPopup.classList.remove('show');
+});
+
+document.addEventListener('click', (event) => {
+  // Check if the click was outside the pop-up
+  if (!infoPopup.contains(event.target) && event.target !== showInfoButton) {
+    infoPopup.classList.remove('show');
+  }
+});
